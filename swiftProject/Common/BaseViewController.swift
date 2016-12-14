853efe875161353512e4bf0a3ed7aed953c248b4
shiqianren <shiqianren@hengtiansoft.com>
2016-12-14 10:33:51 +0800

    宏相关桥文件

---
+++ swiftProject/Common/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  swiftProject
+//
+//  Created by shiqianren on 16/12/14.
+//  Copyright © 2016年 shiqianren. All rights reserved.
+//
+
+import Foundation
+
+class BaseViewController: UIViewController {
+    
+    
+    
+    
+    
+}
